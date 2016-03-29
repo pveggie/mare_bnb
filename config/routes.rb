720c296286fb6e2c12c3fe64cdebaa74fa9110a2
Gemma Gotch <pveggie@hotmail.com>
2016-03-29 14:05:03 +0200

    changed home to stallions page for presentation

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   # from template, don't remove (can be edited depending on page names and action names)
-  root to: 'pages#home'
+  # root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'stallions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
