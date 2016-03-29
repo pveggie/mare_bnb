34b8d8cee5e70bf78bcfa0cf4ad46cff1d6e01de
Gemma Gotch <pveggie@hotmail.com>
2016-03-29 13:58:43 +0200

    carrierwave working now

--- app/models/stallion.rb
+++ app/models/stallion.rb
@@ -1,8 +1,8 @@
 class Stallion < ActiveRecord::Base
-  mount_uploader :photo, PhotoUploader
-
   belongs_to :user
   has_many :availabilities
   has_many :bookings
   has_many :users, through: :bookings
+
+  mount_uploader :photo, PhotoUploader
 end
