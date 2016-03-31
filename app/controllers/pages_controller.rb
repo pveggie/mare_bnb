6987d630d0e8221798385b8f711d12113fb971d5
Gemma Gotch <pveggie@hotmail.com>
2016-03-31 12:04:26 +0200

    homepage beta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @stallions = Stallion.all
   end
 
 # one amazing new comment
--- app/models/stallion.rb
+++ app/models/stallion.rb
@@ -2,6 +2,7 @@ class Stallion < ActiveRecord::Base
   belongs_to :user
   has_many :availabilities
   has_many :bookings
+  validates :user_id, presence: true
   # has_many :users, through: :bookings
 
   mount_uploader :photo, PhotoUploader
