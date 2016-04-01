85f3dc1d1ae1212a3eff89cbb53ea12df7ecb5d4
Gemma Gotch <pveggie@hotmail.com>
2016-04-01 09:05:04 +0200

    small change to seeds so it's not necessary to drop db to run them

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-#
+Stallion.destroy_all
 User.destroy_all
 user_attributes = [
   {
